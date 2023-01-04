1346e81a143183ad7eb22fbb24f8cb5178f2c6fd
Beau <jamesbuchanan0528@gmail.com>
2023-01-03 21:01:57 -0600

    Week 2. Week 3 to come soon.

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,102 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            return vals.Length % 2 == 0;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return number % 2 == 0;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            //return !IsNumberEven(num);
+            return num % 2 != 0;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers is null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+
+
+            return numbers.Max() + numbers.Min();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length >= str2.Length)
+            {
+                return str2.Length;
+            }
+            else
+            {
+                return str1.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers is null)
+            {
+                return 0;
+            }
+            return numbers.Sum();
         }
 
         public int SumEvens(int[] numbers)
         {
+            if (numbers == null)
+            {
+                return 0;
+            }
+            int sum = 0;
+
+
+            foreach (int number in numbers)
+            {
+                if (IsNumberEven(number))
+
+                {
+                    sum += number;
+                }
+            }
+            return sum;
             throw new NotImplementedException();
+            
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
+            if (numbers is null)
+            {
+                return false;
+            }
+            return !IsNumberEven(numbers.Sum());
+
             throw new NotImplementedException();
-        }
+        } 
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+
+            {
+                return 0;
+            }
+            return number / 2;
+            
+            // if odd subtract 1 and / 2
+
+            // if even divide by 2
         }
     }
 }
